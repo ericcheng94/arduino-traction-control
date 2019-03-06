ce3d51bc067d650c2cbb0e21bc3f5dead1ca5187
Eric Cheng <ericmacbook@Erics-MacBook-Pro.local>
2019-03-06 07:19:00 -0800

    fixed attach interrupt not working by fixing typos, setting up attachinterrupt in setup(), and switching pins to pins that support external interrupts

--- arduino_traction_control.ino
+++ arduino_traction_control.ino
@@ -15,15 +15,20 @@
 #define in4 25
 #define enB 3
 // Wheel speed sensor pins
-#define fLeftWheelInterrupt  50
-#define fRightWheelInterrupt 51
-#define rLeftWheelInterrupt  52
-#define rRightWheelInterrupt 53
+// #define fLeftWheelInterrupt  50
+// #define fRightWheelInterrupt 51
+// #define rLeftWheelInterrupt  52
+// #define rRightWheelInterrupt 53
 // Ultrasonic sensor pins
 #define trigPin 43
 #define echoPin 42
 // Traction Control Indicator Light
 #define tcLed 38
+
+const byte fLeftWheelInterrupt = 20;
+const byte fRightWheelInterrupt = 21;
+const byte rLeftWheelInterrupt = 18;
+const byte rRightWheelInterrupt = 19;
 
 long  duration;
 int   distanceCm;
@@ -32,10 +37,10 @@ unsigned long start_time  = 0;
 unsigned long end_time    = 0;
 
 // Set as volatile for use in ISR
-volatile int fLeftSteps  = 0;
-volatile int fRightSteps = 0;
-volatile int rLeftSteps  = 0;
-volatile int rRightSteps = 0;
+volatile byte fLeftSteps  = LOW;
+volatile byte fRightSteps = LOW;
+volatile byte rLeftSteps  = LOW;
+volatile byte rRightSteps = LOW;
 
 volatile int tcRR = 0;
 volatile int tcFR = 0;
@@ -77,13 +82,18 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  pinMode(fLeftWheelInterrupt, INPUT_PULLUP);
-  pinMode(fRightWheelnterrupt, INPUT_PULLUP);
-  pinMode(rLeftWheelnterrupt, INPUT_PULLUP);
-  pinMode(rRightWheelnterrupt, INPUT_PULLUP);
+  pinMode(fLeftWheelInterrupt,  INPUT_PULLUP);
+  pinMode(fRightWheelInterrupt, INPUT_PULLUP);
+  pinMode(rLeftWheelInterrupt,  INPUT_PULLUP);
+  pinMode(rRightWheelInterrupt, INPUT_PULLUP);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(tcLed, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(fLeftWheelInterrupt), frontLeftWheelInterrupted, RISING);
+  attachInterrupt(digitalPinToInterrupt(fRightWheelInterrupt), frontRightWheelInterrupted, RISING);
+  attachInterrupt(digitalPinToInterrupt(rLeftWheelInterrupt), rearLeftWheelInterrupted, RISING);
+  attachInterrupt(digitalPinToInterrupt(rRightWheelInterrupt), rearRightWheelInterrupted, RISING);
   // lcd.setCursor(10, 0);
   // lcd.print("TC:OFF");
   // lcd.setCursor(10, 1);
@@ -98,21 +108,21 @@ void setup() {
   lcd.print("FL:");
 }
 
-// Pin Change Interrupt Request 0
-ISR (PCINT0_vect) {
-	// handle pin change interrupt for selected pins
-
-}
-
-// Pin Change Interrupt Request 1
-ISR (PCINT1_vect) {
-	// handle pin change interrupt for selected pins
-}
-
-// Pin Change Interrupt Request 2
-ISR (PCINT2_vect) {
-	// handle pin change interrupt for selected pins
-}
+// // Pin Change Interrupt Request 0
+// ISR (PCINT0_vect) {
+//   // handle pin change interrupt for selected pins
+//
+// }
+//
+// // Pin Change Interrupt Request 1
+// ISR (PCINT1_vect) {
+//   // handle pin change interrupt for selected pins
+// }
+//
+// // Pin Change Interrupt Request 2
+// ISR (PCINT2_vect) {
+//   // handle pin change interrupt for selected pins
+// }
 
 void tractionControl() {
   start_time = millis();
@@ -125,7 +135,7 @@ void tractionControl() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distanceCm = duration*0.034/2;
-  Serial.println(distanceCm);
+  // Serial.println(distanceCm);
   if (distanceCm < 50)
     motorSpeed = 0;
   else
@@ -133,11 +143,6 @@ void tractionControl() {
 
   while(millis() < end_time)
   {
-	attachInterrupt(digitalPinToInterrupt(fLeftWheelInterrupt),	frontLeftWheelInterrupted, RISING);
-	attachInterrupt(digitalPinToInterrupt(fRightWheelInterrupt), frontRightWheelInterrupted, RISING);
-	attachInterrupt(digitalPinToInterrupt(rLeftWheelInterrupt),	rearLeftWheelInterrupted, RISING);
-	attachInterrupt(digitalPinToInterrupt(rRightWheelInterrupt), rearRightWheelInterrupted, RISING);
-
     if (tcRR > tcFR + 5)
     {
       analogWrite(enA, 0);
@@ -159,34 +164,34 @@ void tractionControl() {
     // lcd.print(tcFR);
     lcd.print(fLeftSteps);
   }
-  // Front left wheel rpm calculation
-  tempFL = fLeftSteps - fLeftSteps_old;
-  fLeftSteps_old = fLeftSteps;
-  rpmFL = ((tempFL * 240 / 20));
-  lcd.setCursor(13, 1);
-  lcd.print(rpmFL);
-  lcd.print(" ");
-  // Front right wheel rpm calculation
-  tempFR = fRightSteps - fRightSteps_old;
-  fRightSteps_old = fRightSteps;
-  rpmFR = ((tempFR * 240 / 20));
-  lcd.setCursor(3, 1);
-  lcd.print(rpmFR);
-  lcd.print(" ");
-  // Rear left wheel rpm calculation
-  tempRL = rLeftSteps - rLeftSteps_old;
-  rLeftSteps_old = rLeftSteps;
-  rpmRL = ((tempRL * 240 / 20));
-  lcd.setCursor(13, 0);
-  lcd.print(rpmRL);
-  lcd.print(" ");
-  // Rear right wheel rpm calculation
-  tempRR = rRightSteps - rRightSteps_old;
-  rRightSteps_old = rRightSteps;
-  rpmRR = ((tempRR * 240 / 20));
-  lcd.setCursor(3, 0);
-  lcd.print(rpmRR);
-  lcd.print(" ");
+  // // Front left wheel rpm calculation
+  // tempFL = fLeftSteps - fLeftSteps_old;
+  // fLeftSteps_old = fLeftSteps;
+  // rpmFL = ((tempFL * 240 / 20));
+  // lcd.setCursor(13, 1);
+  // lcd.print(rpmFL);
+  // lcd.print(" ");
+  // // Front right wheel rpm calculation
+  // tempFR = fRightSteps - fRightSteps_old;
+  // fRightSteps_old = fRightSteps;
+  // rpmFR = ((tempFR * 240 / 20));
+  // lcd.setCursor(3, 1);
+  // lcd.print(rpmFR);
+  // lcd.print(" ");
+  // // Rear left wheel rpm calculation
+  // tempRL = rLeftSteps - rLeftSteps_old;
+  // rLeftSteps_old = rLeftSteps;
+  // rpmRL = ((tempRL * 240 / 20));
+  // lcd.setCursor(13, 0);
+  // lcd.print(rpmRL);
+  // lcd.print(" ");
+  // // Rear right wheel rpm calculation
+  // tempRR = rRightSteps - rRightSteps_old;
+  // rRightSteps_old = rRightSteps;
+  // rpmRR = ((tempRR * 240 / 20));
+  // lcd.setCursor(3, 0);
+  // lcd.print(rpmRR);
+  // lcd.print(" ");
 
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
@@ -199,7 +204,7 @@ void tractionControl() {
   }
   tcRR = 0;
   tcFR = 0;
-  Serial.println(motorSpeed);
+  // Serial.println(motorSpeed);
 }
 
 void loop() {
@@ -208,21 +213,22 @@ void loop() {
 
 // External ISR triggered by a change in the front left wheel sensor pin state
 void frontLeftWheelInterrupted() {
-	fLeftStep++;
-	tcGO = 1;
+  fLeftSteps++;
+  // Serial.println("FLINT");
+  tcGO = 1;
 }
 // External ISR triggered by a change in the front right wheel sensor pin state
 void frontRightWheelInterrupted() {
-	fRightSteps++;
-	tcFR++;
-	tcGO = 1;
+  fRightSteps++;
+  tcFR++;
+  tcGO = 1;
 }
 // External ISR triggered by a change in the rear left wheel sensor pin state
 void rearLeftWheelInterrupted() {
-	rLeftSteps++;
+  rLeftSteps++;
 }
 // External ISR triggered by a change in the rear right wheel sensor pin state
 void rearRightWheelInterrupted() {
-	rRightSteps++;
-	tcRR++;
+  rRightSteps++;
+  tcRR++;
 }
